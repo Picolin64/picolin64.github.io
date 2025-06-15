331bdce5857b64484a573dcb4f69328cefa0fe4c
David C <davidcg_23-2@hotmail.com>
2025-06-15 12:17:01 -0500

    style: improve style of inline code and add border to dropdown text  (also improve writeup metadata)

--- pages/writeups/[slug].vue
+++ pages/writeups/[slug].vue
@@ -1,7 +1,7 @@
 <template>
   <main class="min-h-screen">
     <div
-      class="prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden prose-code:text-base prose-code:bg-gray-300 dark:prose-code:bg-gray-700 prose-blockquote:not-italic before:[&_blockquote_p:first-of-type]:content-none after:[&_blockquote_p:last-of-type]:content-none prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg max-w-5xl"
+      class="prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden prose-code:text-base prose-code:bg-gray-300 dark:prose-code:bg-gray-700 prose-blockquote:not-italic before:[&_blockquote_p:first-of-type]:content-none after:[&_blockquote_p:last-of-type]:content-none prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg prose-blockquote:border-lime-500 max-w-5xl"
     >
       <ContentDoc v-slot="{ doc }" tag="writeup">
         <article>
@@ -16,13 +16,14 @@
 const route = useRoute();
 const { slug } = route.params;
 useSeoMeta({
+  ogType: "website",
   ogImage: `https://picolin64.github.io/writeups/${slug}/thumbnail.png`,
+  ogUrl: `https://picolin64.github.io/writeups/${slug}/`,
   twitterCard: "summary_large_image",
   writeupAuthor: "Picolin64",
 });
 </script>
 <style>
-.prose h2 a,
 .prose h3 a,
 .prose h4 a {
   @apply no-underline;
@@ -31,5 +32,16 @@ useSeoMeta({
   display: block;
   margin-left: auto;
   margin-right: auto;
+}
+details {
+  padding: 9px;
+  border: 2px solid rgba(132, 204, 22, 0.6);
+  border-radius: 10px;
+}
+code[data-v-inspector="node_modules/@nuxt/content/dist/runtime/components/Prose/ProseCodeInline.vue:2:3"]{
+  padding-left: 4px;
+  padding-right: 4px;
+  padding-bottom: 2px;
+  border-radius: 5px;
 }
 </style>
