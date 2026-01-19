b24e3e03756b7d5465b668fe4bcac852c5cba196
David C <davidcg_23-2@hotmail.com>
2026-01-18 20:01:52 -0500

    chore: update copyright year to 2026

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -3,6 +3,6 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
   >
     <br>
-    <p>© 2025 Picolin64. Todos los derechos reservados.</p>
+    <p>© 2026 Picolin64. Todos los derechos reservados.</p>
   </footer>
 </template>
